f5798e10103abab08c482024fe5f1535d694a7df
kaviza90 <vieralaqueviera@gmail.com>
2019-10-02 18:13:02 -0500

    se agrega el componente y llamada Fetch

---
+++ src/components/Fetch.js
@@ -0,0 +1,36 @@
+import React from "react"; 
+
+export default class FetchApi extends React.Component {
+
+    state = {
+        loading: true,
+        name: null,
+    }
+
+async componentDidMount() {
+const url = 'https://rickandmortyapi.com/api/character/';
+const response = await fetch(url);
+const data = await response.json();
+this.setState({name: data.results[0], loading: false });
+console.log(data.results[0]);
+    }
+
+    render() {
+if (this.state.loading) {
+    return <div>loading...</div>
+}
+
+if (!this.state.name)
+return <div>No es un nombre</div>
+
+ return (
+<div>
+    <div>
+    <div>
+        <div>{this.state.name}</div>
+    </div>          
+    )}
+    </div>
+        );
+    }
+}
