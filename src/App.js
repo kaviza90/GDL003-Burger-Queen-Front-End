07c9e642fe4fff3197250160052a36c91caea224
PaulinaFranco <dadiante@gmail.com>
2019-10-14 15:44:05 -0500

    deploy nuevo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <HashRouter>
          <Switch>
-            <Route exact path="/VeganFood" component={VeganFood}/>
+            <Route exact path="/" component={VeganFood}/>
         </Switch>
         </HashRouter>
   );
